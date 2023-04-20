55de35347ebcdbf8e637dcf0fffd1d003b4a10b8
Lara Livera <laralivera@Laras-MacBook-Pro.local>
2023-04-20 18:08:51 -0400

    auth done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,10 +4,14 @@ import { Link } from 'react-router-dom'
 export default function NavBar(){
     return (
         <nav>
-            <Link to="">New & Noteworthy</Link>;
-            <Link to="">Mood</Link>&nbsp;
-            <Link to=""><img src="./logo192.png" alt="Home" /></Link>&nbsp;
-            <Link to="">Search</Link>&nbsp;
+            <Link to="">New & Noteworthy</Link>
+            &nbsp;&nbsp;
+            <Link to="">Mood</Link>
+            &nbsp;&nbsp;
+            <Link to=""><img src="./logo192.png" alt="Home" /></Link>
+            &nbsp;&nbsp;
+            <Link to="">Search</Link>
+            &nbsp;&nbsp;
             <Link to="">MyAnime</Link>
         </nav>
     );
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,20 +1,21 @@
 import './App.css';
 import NavBar from '../../components/NavBar/NavBar';
-
-
-
-
-
-
-
-
+import AuthPage from '../AuthPage/AuthPage';
+import React, { useState } from 'react';
+import { getUser } from '../../utilities/users-service';
 
 
 export default function App() {
+  const [user, setUser] = useState(getUser());
   return (
     <main className="App">
+      { user ?
+      <>
       <NavBar/>
-      <h1>Anime Moods</h1>
+      </>
+      :
+      <AuthPage setUser={setUser} />
+      }
     </main>
   );
 }
