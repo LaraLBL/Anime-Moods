db87483237ab35d29673a5904aaf78a3d7369aa0
DianaDPC <77179386+DianaDPC@users.noreply.github.com>
2023-04-23 16:30:32 -0400

    Update animeData.js

--- animeData.js
+++ animeData.js
@@ -5,7 +5,7 @@ const url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmet
 const options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '3c8a17f6eemshb6c02b77ea8c1cdp1d6981jsnf0b66f21b11c',
+    'X-RapidAPI-Key': 'process.env.ANIME_DB_KEY',
     'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
   }
 };
