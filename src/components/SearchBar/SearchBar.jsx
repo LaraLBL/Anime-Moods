d27b16626875b59f30ec67e60880b104b2eb0f08
Lara Livera <laralivera@Laras-MacBook-Pro.local>
2023-04-24 17:49:34 -0400

    stash

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,35 +1,36 @@
 import React, {useState} from 'react';
-import { createCard } from "../../components/AnimeCard/AnimeCard.jsx";
+import { createCard } from "../AnimeCard/AnimeCard.jsx";
 
 export default function SearchBar ()  {
- const [searchInput, setSearchInput] = useState("");
- const anime = [{anime.titile}];
+ const [input, setInput] = useState("");
+ 
+ const fetchData = (value) => {
+  fetch("")
+  .then((response) => response.json())
+  .then?((json) => {}
+  )
+ };
 
- const handleChange = (e) => {
-  e.preventDefault();
-  setSearchInput(e.target.value);
+ const handleChange = (value) => {
+  setInput(value)
+  fetchData(value)
  };
  
- if (searchInput.length > 0) {
-  anime.filter((anime) => {
-  return anime.match(searchInput);
-  });
- }
 
- return <div>
-
-  <input
-   placeholder="Search title"
-   type="search"
-   onChange={handleChange}
-   value={searchInput} 
-  />
-   <main className='sidemargin'>
-    <div className="columns is-multiline">
-    {data.map(createCard)}
-    </div>
-   </main>
-
- </div>
-}
+ return (
+  <div>
+    <input
+    placeholder="Search by title"
+    type="search-box"
+    onChange={(e) => handleChange(e.target.value)}
+    value={input} 
+    />
+    <main className='sidemargin'>
+      <div className="columns is-multiline">
+      {data.map(createCard)}
+      </div>
+    </main>
+  </div>
+ );
+};
 
